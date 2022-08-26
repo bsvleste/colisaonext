e747260af6fc031c7d45c8a6a818399604d32cde
bsvleste <bvaleiro@gmail.com>
2022-08-26 13:05:49 -0300

    arrumando storyboard socoreboar

--- src/components/Scoreboard/stories.tsx
+++ src/components/Scoreboard/stories.tsx
@@ -1,19 +1,16 @@
-import { Story, Meta } from '@storybook/react'
-import Scoreboard, { ScoreboardProps } from '.'
+import { Story, Meta } from '@storybook/react';
+import Scoreboard, { ScoreboardProps } from '.';
 
 export default {
   title: 'Scoreboard',
-  component: Scoreboard
-} as Meta
+  component: Scoreboard,
+} as Meta;
 export const Default: Story<ScoreboardProps> = (args) => (
   <div style={{ margin: '0 auto' }}>
     <Scoreboard {...args} />
   </div>
-)
+);
 
 Default.args = {
   data: '15/05/2021',
-  color: 'amareloMenu',
-  background: 'preto',
-  infoQuadro: 'Segundo Quadro'
-}
+};
